3922851008884ed52ebf3dac982f81b82c7f5874
atut <atut@pas.com.np>
2019-07-18 22:58:13 +0545

    Completed First Chapter, Variable Types

---
+++ Learning Phase/1_variabletypes.py
@@ -0,0 +1,144 @@
+"""
+Atut Gorkhali 
+Masters Of Computer Information System
+Nepal
+"""
+
+
+"""
+Python Variable Types
+--------------
+Standard
+--------------
+# Numbers
+# String
+# List
+# Tuple
+# Dictionary 
+--------------
+"""
+"""Assignment
+"""
+# #-------------------
+# myInteger   =   5000
+# myFloat     =   5000.12
+# myString    =   "Atut Gorkhali"     
+
+# print(myInteger)
+# print(myFloat)
+# print(myString)
+
+"""Multiple Assignment
+"""
+# #------------------------
+# firstValue  =   secondValue =   thirdValue  =   1
+# valueSet1,valueSet2,valueSet3    =   1,  1.11,   "Atut Gorkhali"
+# print(firstValue)
+# print(thirdValue)
+# print(valueSet1)
+# print(valueSet3)
+
+"""Numbers
+-------------------
+Supported Numerical Types
+-------------------
+# int
+# long
+# float
+# complex
+-------------------
+Number objects are created when value is assigned to variable
+"""
+# # number value assignment
+# value1  =   1
+# value2  =   2
+# # deleting number object
+# del value1
+
+"""Strings
+"""
+# # Assignment
+# myString    =   "Atut Gorkhali"
+# # print operions on myString for demo
+# print (myString)
+# print(myString.upper())
+# print(myString.lower())
+# print(myString[0])
+# print(myString[2:4])
+# print(myString[2:])
+# print(myString[:4])
+# print(myString * 2)
+# print(myString + " is printed!")
+# #-------------------------------
+
+
+"""Lists
+"""
+# #---------- Assignment of List -------------
+# myPersonalList      =   ["Atut Gorkhali","A+",1,182804,85000]
+# myCollegeList =   ["MCIS", "NCIT"]
+
+# print (myPersonalList)
+# print (myCollegeList)
+
+# print(myCollegeList[0])
+# print(myPersonalList[1:])
+# print(myPersonalList[1:3])
+# print(myPersonalList[:3])
+# print(myPersonalList * 2)
+# print(myPersonalList + myCollegeList)
+
+"""Tuple
+"""
+# #------------ Assignment ---------------
+# myCollegeTuple      =   ("Atut Gorkhali", "NCIT", "MCIS", 182804, "A+")
+# myProfessionalTuple =   ("Software Engineer","Infodevelopers")
+# #-------------Print Test ---------------
+# print (myCollegeTuple)
+# print (myProfessionalTuple)
+# print (myCollegeTuple[0])
+# print (myCollegeTuple[2:])
+# print (myCollegeTuple[2:3])
+# print (myCollegeTuple[:3])
+# print (myCollegeTuple * 2)
+# print (myCollegeTuple + myProfessionalTuple)
+
+"""
+----------------------
+Tuple VS List
+----------------------
+"""
+# Tuple are read-only.
+# Tuple can not be updated, but List can be updated as well.
+
+"""Dictionary
+"""
+# # -------- Assignment -------------
+# myDictionary            =   {}
+# myDictionary[1]         =   1
+# myDictionary['name']    =    "Atut Gorkhali"
+# myDictionary['address'] =    "kathmandu"
+
+# # -------- Print Test -------------
+
+# print (myDictionary)
+# print (myDictionary[1])
+# print (myDictionary['name'])
+# print(myDictionary.keys())
+# print(myDictionary.values())
+
+"""Type Conversion
+"""
+# myVariable = 123456789
+# print(myVariable)
+# print (float(myVariable))
+# print (oct(myVariable))
+# print (hex(myVariable))
+# print (str(myVariable))
+
+
+
+
+
+
+
