9c9ffea1609d1be40cda44a80e178bd2cfdd266f
atut <atut@pas.com.np>
2019-07-20 21:34:09 +0545

    Basic Operators

---
+++ Learning Phase/2_basicOperators.py
@@ -0,0 +1,67 @@
+"""Types of Operator
+-----------------------
+# Arithmetic Operators
+# Comparison (Relational) Operators
+# Assignment Operators
+# Logical Operators
+# Bitwise Operators
+# Membership Operators
+# Identity Operators
+"""
+#------Arithmetic Operators--------
+# Addition          (+)
+# Substraction      (-)
+# Multiplication    (*)
+# Division          (/)
+# Modulus           (%)
+# Exponent          (**)
+# Floor Division    (//)
+
+# variable1 = 1.1
+# variable2 = 2.2
+
+# print(variable1+variable2)
+# print(variable1-variable2)
+# print(abs(variable1-variable2))
+# print(variable1*variable2)
+# print(variable1/variable2)
+# print(variable1%variable2)
+# print(variable1**variable2)
+# print(variable1//variable2)
+
+
+# -----------------------------------
+# --- Python Comparison Operators----
+# Equals to                 ==        
+# Not Equals to             !=
+# Greater Than              >
+# Less Than                 <
+# Greater Than or Equal to  >=
+# Less Than or Equal to     <=
+
+# variable1 = 1.1
+# variable2 = 2.2
+# print (variable1==variable2)
+# print (variable1!=variable2)
+# print (variable1>variable2)
+# print (variable1<variable2)
+# print (variable1<=variable2)
+# print (variable1>=variable2)
+
+
+
+# ------ Python Membership Operators ------
+# IN            (in)
+# NOT IN        (not in)
+#----------------------------
+# print (1 not in {1,2,3})
+# print (1 in [1,2,3])
+
+# --------- Python Identity Operators -------
+# IS            (is)
+# IS NOT        (is not)
+#----------------------------
+# testVar= [0,1,2,3,4,5]
+# print (1 is testVar[1])
+# print (6 is not testVar[1])
+
